057f0e15ba617cf4495bdd3e0318cb95df360605
JustDalo <shishlodan@mail.ru>
2021-10-10 21:33:09 +0300

    test library

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace TestLibrary
+{
+    public class Class1
+    {
+        public double MyDouble { get; set; }
+
+        public int MyMethod(int param)
+        {
+            return param;
+        }
+    }
+}
