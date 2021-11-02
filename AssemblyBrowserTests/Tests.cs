073aaf3a8c884d4da73a2968d95cc502a8fab253
JustDalo <shishlodan@mail.ru>
2021-11-02 22:03:53 +0300

    tests

--- AssemblyBrowserTests/Tests.cs
+++ AssemblyBrowserTests/Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AssemblyBrowserLibrary;
+using AssemblyBrowserLibrary.AssemblyCompositionElements;
 using NUnit.Framework;
 
 namespace AssemblyBrowserTests
@@ -8,22 +9,12 @@ namespace AssemblyBrowserTests
     public class Tests
     {
         private readonly AssemblyBrowser _assemblyBrowser = new AssemblyBrowser();
-        private MyClass _myClass = new MyClass();
 
         [Test]
         public void Test1()
         {
-            _assemblyBrowser.GetNamespace(@"C:\Users\ASUS\RiderProjects\MPPproject3\TestLibrary\bin\Debug\TestLibrary.dll");
-        }
-    }
-    
-    public class MyClass
-    {
-        public double MyDouble { get; set; }
-
-        public int MyMethod(int param)
-        {
-            return param;
+            AssemblyContainerInfo[] assembly = _assemblyBrowser.GetNamespace(@"C:\Users\ASUS\RiderProjects\MPPproject3\TestLibrary\bin\Debug\TestLibrary.dll");
+            Assert.NotNull(assembly);
         }
     }
 }
